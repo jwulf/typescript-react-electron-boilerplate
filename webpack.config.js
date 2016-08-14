1f9326e2fac57cb349cdf24ed964a75b555a418e
David Terry <davidterry@posteo.de>
2016-08-14 20:07:22 +0200

    Webpack, typescript, and react hello world

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+
+var config = {
+
+  entry: [
+      "./src/app.tsx"
+  ],
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+
+  resolve: {
+    extensions: ["", ".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
