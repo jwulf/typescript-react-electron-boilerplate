fae0e96667ecd69f200c2e13f7b56d679460c9af
David Terry <davidterry@posteo.de>
2016-08-27 15:36:13 +0200

    HMR working inside webpack

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let win
 function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
-  win.loadURL(`file://${__dirname}/build/index.html`)
+  win.loadURL(`http://localhost:3000/`)
 
   win.webContents.openDevTools()
 
